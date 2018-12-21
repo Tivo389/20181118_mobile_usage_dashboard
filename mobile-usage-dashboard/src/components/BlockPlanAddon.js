31d18a377fe4b9bef380476873730a321e329ca8
Shun Miyaki <shunmiyaki@hotmail.com>
2018-12-20 22:02:59 -0800

    renderActiveComponent Complete

--- mobile-usage-dashboard/src/components/BlockPlanAddon.js
+++ mobile-usage-dashboard/src/components/BlockPlanAddon.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import BlockPlanUsage from './BlockPlanUsage';
-import BlockInfo from './BlockInfo';
-import BlockTitle from './BlockTitle';
 
 class BlockPlanAddon extends Component {
   render() {
